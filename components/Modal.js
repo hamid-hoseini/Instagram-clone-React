07d6867e99363615425cbc1f5658378dfce57cae
Hamid <mail.hamid.hoseini@gmail.com>
2022-08-26 21:33:49 -0700

    upload image and caption to firebase and firestore

--- components/Modal.js
+++ components/Modal.js
@@ -3,12 +3,18 @@ import { modalState } from '../atoms/modalAtom';
 import { Dialog, Transition } from '@headlessui/react'
 import { Fragment, useRef, useState } from 'react';
 import { CameraIcon } from '@heroicons/react/outline';
+import { db, storage} from '../firebase';
+import { addDoc, collection, serverTimestamp, updateDoc, doc } from "@firebase/firestore"
+import { useSession } from 'next-auth/react';
+import { ref, getDownloadURL, uploadString } from "@firebase/storage"
 
 function Modal() {
+  const { data: session } = useSession();
   const [open, setOpen] = useRecoilState(modalState);
   const filePickerRef = useRef(null);
   const captionRef = useRef(null);
   const [selectedFile, setSelectedFile] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const addImpageToPost = (e) => {
     const reader = new FileReader();
@@ -21,13 +27,45 @@ function Modal() {
     };
   }; 
 
+  const uploadPost = async() => {
+    if (loading) return;
+
+    setLoading(true);
+    
+    // 1) Create a post and add to firestore 'posts' collection
+    // 2) Get the post ID for the newly created posts
+    // 3) upload the image to firebase storage with the post ID
+    // 4) Get a download URL fb storage and update the original post with image
+
+    const docRef = await addDoc(collection(db, 'posts'), {
+      username: session.user.username,
+      caption: captionRef.current.value,
+      profileImg: session.user.image,
+      timestamp: serverTimestamp()
+    });
+
+    const imageRef = ref(storage, `posts/${docRef.id}/image`);
+
+    await uploadString(imageRef, selectedFile, 'data_url').then(async snapshot => {
+      const downloadURL = await getDownloadURL(imageRef);
+
+      await updateDoc(doc(db, 'posts', docRef.id), {
+        image: downloadURL
+      })
+    });
+
+    setOpen(false);
+    setLoading(false);
+    setSelectedFile(null);
+  }
+
   return <Transition.Root show={open} as={Fragment}>
     <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={setOpen}>
       <div className='flex items-end justify-center min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0'>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
-          enterForm="opacity-0"
+          enterFrom="opacity-0"
           enterTo='opacity-100'
           leave="ease-in duration-200"
           leaveFrom='opacity-100'
@@ -44,7 +82,7 @@ function Modal() {
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
-          enterForm="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           enterTo='opacity-100 translate-y-0 sm:scale-100'
           leave="ease-in duration-200"
           leaveFrom='opacity-100 translate-y-0 sm:scale-100'
@@ -91,8 +129,10 @@ function Modal() {
                 className='inline-flex justify-center w-full rounded-md border border-transparent shadow-sm
                 px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none
                 focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:text-sm disabled:bg-gray-300
-                disabled:cursor-not-allowed hover:disabled:bg-gray-300'>
-                  Upload Post
+                disabled:cursor-not-allowed hover:disabled:bg-gray-300'
+                 onClick={uploadPost} disabled={!selectedFile}
+              >
+                {loading ? "Uploading..." : "Upload Post"}
               </button>
             </div>
           </div>
--- firebase.js
+++ firebase.js
@@ -2,6 +2,8 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+//import { getStorage } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
